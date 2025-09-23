f2d9033c61606f3068d12dd004092ceb38f46ccd
yusufGM <muhamadmaulanayusuf73@gmail.com>
2025-09-23 18:30:05 +0700

    Hapus semua isi repo

--- tapako-frontend/src/components/store/filterStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useFilterStore = create((set) => ({
-  filters: { sale: false, gender: 'all', ageGroup: 'all', price: 'none' },
-  setFilter: (key, value) => set((state) => ({ filters: { ...state.filters, [key]: value } })),
-  toggleFilter: (key) => set((state) => ({ filters: { ...state.filters, [key]: !state.filters[key] } })),
-}));
